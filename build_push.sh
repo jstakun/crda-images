5c5aa72271f8b56f377779ef40c4e7289e4599ed
jstakun <jstakun@redhat.com>
2022-04-20 08:45:39 +0200

    build_push script

---
+++ build_push.sh
@@ -0,0 +1,12 @@
+VERSION_MAJOR=11
+VERSION_MINOR=11-6
+
+podman login quay.io
+
+podman build -f ./Containerfile.openjdk -t dev.local/jstakun/crda-openjdk:$VERSION_MAJOR --squash
+podman tag dev.local/jstakun/crda-openjdk:11 quay.io/jstakun/crda-openjdk:$VERSION_MINOR
+podman tag dev.local/jstakun/crda-openjdk:11 quay.io/jstakun/crda-openjdk:$VERSION_MAJOR
+
+podman push quay.io/jstakun/crda-openjdk:$VERSION_MINOR
+podman push quay.io/jstakun/crda-openjdk:$VERSION_MAJOR
+
