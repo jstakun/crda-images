c92baad71a1627b9d255b4833da3aa8c2278d90b
jstakun <jstakun@redhat.com>
2022-06-14 15:10:35 +0200

    update

--- build_push.sh
+++ build_push.sh
@@ -1,12 +1,19 @@
 VERSION_MAJOR=11
-VERSION_MINOR=11-7
+VERSION_MINOR=11-9
 
 podman login quay.io
 
 podman build -f ./Containerfile.openjdk -t dev.local/jstakun/crda-openjdk:$VERSION_MAJOR --squash
-podman tag dev.local/jstakun/crda-openjdk:11 quay.io/jstakun/crda-openjdk:$VERSION_MINOR
-podman tag dev.local/jstakun/crda-openjdk:11 quay.io/jstakun/crda-openjdk:$VERSION_MAJOR
 
-podman push quay.io/jstakun/crda-openjdk:$VERSION_MINOR
-podman push quay.io/jstakun/crda-openjdk:$VERSION_MAJOR
+RS=$?
 
+if [ $RS == 0 ]
+then
+  podman tag dev.local/jstakun/crda-openjdk:11 quay.io/jstakun/crda-openjdk:$VERSION_MINOR
+  podman tag dev.local/jstakun/crda-openjdk:11 quay.io/jstakun/crda-openjdk:$VERSION_MAJOR
+
+  podman push quay.io/jstakun/crda-openjdk:$VERSION_MINOR
+  podman push quay.io/jstakun/crda-openjdk:$VERSION_MAJOR
+else
+  echo "Skipped pushing due to error"
+fi
